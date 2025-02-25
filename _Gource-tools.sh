27ce52fe75ad964919bf6c3f8cb5ee146b6638ce
Bitpaint <bitpaintclub@gmail.com>
2025-02-25 19:36:42 +0100

    a
    a

--- _Gource-tools.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-cd ./src
-cd ./menu
-source ./gource-tools-main.sh
--- src/repos_dl.sh
+++
@@ -1,6 +0,0 @@
-grep . ../_List.txt | while read line ; do
-    cd repos;
-    git clone --quiet "$line";
-    echo "  Cloning $line"
-    cd ..;
-  done
